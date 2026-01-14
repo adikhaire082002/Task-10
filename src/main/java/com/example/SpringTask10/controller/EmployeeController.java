93f4f6020ddb5edfeac3021a6a239f8ae24570d5
Aditya Khaire <adikhaire082002@gmail.com>
2026-01-14 17:55:01 +0530

    SpringTask11 added

--- src/main/java/com/example/SpringTask10/controller/EmployeeController.java
+++ src/main/java/com/example/SpringTask10/controller/EmployeeController.java
@@ -4,6 +4,7 @@ package com.example.SpringTask10.controller;
 import com.example.SpringTask10.DTO.EmployeeDto;
 import com.example.SpringTask10.service.EmployeeService;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("employee")
+@Slf4j
 public class EmployeeController {
 
     @Autowired
@@ -20,24 +22,28 @@ public class EmployeeController {
     @PostMapping("/add")
     public ResponseEntity<?> add(@RequestBody @Valid EmployeeDto employeeDto) {
         employeeService.add(employeeDto);
+        log.info("Employee added successfully");
         return new ResponseEntity<>("Employee added",HttpStatus.CREATED);
     }
 
     @GetMapping("/getById")
     public ResponseEntity<?> getById(@RequestParam Integer id) {
         EmployeeDto employeeDto = employeeService.get(id);
+        log.info("Employee found successfully");
         return new ResponseEntity<>(employeeDto,HttpStatus.FOUND);
     }
 
     @PutMapping("/update")
     public ResponseEntity<?> update(@RequestBody @Valid EmployeeDto employeeDto, @RequestParam Integer id) {
         employeeService.update(employeeDto,id);
+        log.info("Employee updated successfully");
         return new ResponseEntity<>("Employee updated",HttpStatus.OK);
     }
 
     @DeleteMapping("/delete")
     public ResponseEntity<?> delete(@RequestParam Integer id) {
         employeeService.delete(id);
+        log.info("Employee deleted successfully");
         return new ResponseEntity<>("Employee deleted",HttpStatus.OK);
     }
 }
--- src/main/java/com/example/SpringTask10/service/EmployeeService.java
+++ src/main/java/com/example/SpringTask10/service/EmployeeService.java
@@ -5,10 +5,12 @@ import com.example.SpringTask10.entity.Employee;
 import com.example.SpringTask10.exception.ResourceNotFound;
 import com.example.SpringTask10.mapper.EmployeeMapper;
 import com.example.SpringTask10.repositoy.EmployeeRepo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class EmployeeService {
 
     @Autowired
@@ -22,26 +24,35 @@ public class EmployeeService {
         try{
             employeeRepo.save(employee);
         } catch (RuntimeException e) {
+            log.error("Error occurred for saving employee {} ",e.getMessage());
             throw new RuntimeException(e.getMessage());
         }
     }
 
     public EmployeeDto get(Integer id) {
-        Employee employee = employeeRepo.findById(id).orElseThrow(() -> new ResourceNotFound("Employee not found"));
+        Employee employee = employeeRepo.findById(id).orElseThrow(() -> {
+            log.error("Employee not found");
+            return new ResourceNotFound("Employee not found");
+        });
         return employeeMapper.toEmployeeDto(employee);
     }
 
     public void delete(Integer id) {
-        Employee employee = employeeRepo.findById(id).orElseThrow(() -> new ResourceNotFound("Employee not found"));
+        Employee employee = employeeRepo.findById(id).orElseThrow(() ->{
+            log.error("Employee not found");
+            return new ResourceNotFound("Employee not found");});
         employeeRepo.delete(employee);
     }
 
     public void update(EmployeeDto employeeDto, Integer id) {
-        Employee employee = employeeRepo.findById(id).orElseThrow(() -> new ResourceNotFound("Employee not found"));
+        Employee employee = employeeRepo.findById(id).orElseThrow(() ->{
+            log.error("Employee not found");
+            return new ResourceNotFound("Employee not found");});
         Employee employee1 = employeeMapper.toEmployee(employeeDto, employee);
         try{
             employeeRepo.save(employee1);
         } catch (RuntimeException e) {
+            log.error("Error occurred for update employee {} ",e.getMessage());
             throw new RuntimeException(e.getMessage());
         }
     }
